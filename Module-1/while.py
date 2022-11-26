f3729286f4c9909cc9d1e8e9874e42c2d73a7397
SanketTOPS <sanket.tops@gmail.com>
2022-11-26 11:22:10 +0530

    Loops

---
+++ Module-1/while.py
@@ -0,0 +1,23 @@
+"""i=1 #initalization
+
+while i<=100: #condition (true)
+    #statmenet (body of loop)
+    print(i)
+    i+=1 #i=i+1 #inc/dec"""
+
+
+"""i=2
+while i<=10:
+    print(i)
+    i+=2"""
+
+
+"""i=10
+while i>=1:
+    print(i)
+    i-=1"""
+
+i=1
+while i<=100:
+    print("Sanket = ",i) #statement
+    i+=1
