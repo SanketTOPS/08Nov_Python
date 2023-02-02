a50cde151c7b58e4199ecfd4b8b6e3afe703244c
SanketTops <sanket.tops@gmail.com>
2023-02-02 11:40:38 +0530

    DBApp

--- Django Project/DBProject/myapp/views.py
+++ Django Project/DBProject/myapp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .forms import signupForm
+from .models import usersignup
 
 # Create your views here.
 def index(request):
+    if request.method=='POST':
+        newuser=signupForm(request.POST)
+        if newuser.is_valid():
+            newuser.save()
+            print("Your data has been saved!")
+        else:
+            print(newuser.errors)
     return render(request,'index.html')
+
+def showdata(request):
+    data=usersignup.objects.all()
+    return render(request,'showdata.html',{'data':data})
