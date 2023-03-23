cd92e5992fb2ac49789dd8a489529ac2acddff6a
SanketTops <sanket.tops@gmail.com>
2023-03-23 11:23:50 +0530

    API POST and DELETE

--- Django Project/APIProject/apiapp/urls.py
+++ Django Project/APIProject/apiapp/urls.py
@@ -4,4 +4,7 @@ from apiapp import views
 
 urlpatterns = [
     path('getalldata/',views.getalldata),
+    path('getid/<int:id>/',views.getid),    
+    path('saveuser/',views.saveuser),
+    path('deleteuser/<int:id>/',views.deleteuser),
 ]
--- Django Project/APIProject/apiapp/views.py
+++ Django Project/APIProject/apiapp/views.py
@@ -3,6 +3,7 @@ from .models import userInfo
 from rest_framework.decorators import api_view
 from .serializers import userSerialziers
 from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
@@ -11,6 +12,42 @@ def getalldata(request):
     userdata=userInfo.objects.all()
     userserial=userSerialziers(userdata,many=True)
     return Response(userserial.data)
+
+@api_view(['GET'])
+def getid(request,id):
+    try:
+        stid=userInfo.objects.get(id=id)
+    except userInfo.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    userserial=userSerialziers(stid)
+    return Response(userserial.data)
+    
+
+@api_view(['POST'])
+def saveuser(request):
+    if request.method=='POST':
+        userserial=userSerialziers(data=request.data)
+        if userserial.is_valid():
+            userserial.save()
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET','DELETE'])
+def deleteuser(request,id):
+    try:
+        stid=userInfo.objects.get(id=id)
+    except userInfo.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method=="GET":
+        userserial=userSerialziers(stid)
+        return Response(userserial.data)
+    if request.method=='DELETE':
+        userInfo.delete(stid)
+        return Response(status=status.HTTP_202_ACCEPTED)
+
 
 
 
